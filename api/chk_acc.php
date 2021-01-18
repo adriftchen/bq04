d3c9c4cb7848f959ee41d61932b2ce552c15721b
linkzepp <adriftchen@gmail.com>
2021-01-18 13:52:21 +0800

    完成會員註冊-chk_acc.php修正

--- api/chk_acc.php
+++ api/chk_acc.php
@@ -1,9 +1,11 @@
 <?php
 include_once "../base.php";
 
+// echo $Mem->count(['acc'=>$_GET['acc']]);
+
 $acc=$_GET['acc'];
 
-$chk=$Mem->count(['acc=>$acc']); /* 無帳號>0 有帳號>1 */
+$chk=$Mem->count(['acc'=>$acc]); /* 無帳號>0 有帳號>1 */
 
-return $chk;
+echo $chk;
 ?>
