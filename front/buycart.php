8aced3eb373578067e67d07f5fc520116bb3e77e
linkzepp <adriftchen@gmail.com>
2021-01-29 10:41:23 +0800

    完成購物車刪除商品功能

--- front/buycart.php
+++ front/buycart.php
@@ -1,4 +1,20 @@
 <?php
+//若顯示出現 "header has already been declared..." 重複發送錯誤
+//將此段程式碼移到index.php，並在最外面加上一層判斷:
+// <?php include_once "base.php";
+
+// if(isset($_GET['do']) && $_GET['do']=='buycart'){
+
+//     if(isset($_GET['goods'])){
+//         $_SESSION['cart'][$_GET['goods']]=$_GET['qt'];
+//     }
+
+//     if(empty($_SESSION['mem'])){
+//         to("../index.php?do=login");
+//         exit();
+//     }
+// }
+
 
 //判斷有無已加入的商品
 if(isset($_GET['goods'])){
@@ -44,7 +60,7 @@ if(empty($_SESSION['cart'])){
   <td><?=$g['quota'];?></td>
   <td><?=$g['price'];?></td>
   <td><?=$qt*$g['price'];?></td>
-  <td><img src="icon/0415.jpg" alt=""></td>
+  <td><img src="icon/0415.jpg" onclick="delItem(<?=$id;?>)"></td>
   </tr>
 <?php
 
@@ -55,3 +71,13 @@ if(empty($_SESSION['cart'])){
   <a href="index.php"><img src="icon/0411.jpg" alt=""></a>
   <a href="?do=checkout"><img src="icon/0412.jpg" alt=""></a>
 </div>
+
+<script>
+function delItem(id){
+  $.post("api/del_item.php",{id},function(){
+    // location.reload();
+    //用reload最後一筆商品會刪不掉
+    location.href="?do=buycart";
+  })
+}
+</script>
